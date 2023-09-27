35b13e32f2d8eeeaee40ab18948e1451994292fd
NatawatOhm <natawat.cho@gmail.com>
2023-09-27 14:23:58 +0700

    [config]vue router

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Main from '../pages/Main.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: Main,
+  },
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+  scrollBehavior() {
+    return { top: 0 }
+  },
+})
+router.afterEach((to, from) => {
+  const toDepth = to.meta.permission || 0
+  const fromDepth = from.meta.permission || 0
+  to.meta.leaveClass = fromDepth < toDepth ? 'animate__animated animate__fadeOut animate__fast' : 'animate__animated animate__slideOutRight animate__fast'
+  to.meta.enterClass = fromDepth < toDepth ? 'animate__animated animate__slideInRight animate__fast' : 'animate__animated animate__fadeIn animate__fast'
+})
+export default router
